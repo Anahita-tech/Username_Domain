e3239ace7e379fb3ca024c8b4eff8677731f4af5
Anahita-tech <anahitaghasemi20@gmail.com>
2025-07-16 13:47:56 +0330

    Update username_domain.py

--- username_domain.py
+++ username_domain.py
@@ -2,6 +2,6 @@ a=input('Enter an email please:') # Get email input from user
 if '@' not in a: # Check if '@' is not present in the email
     print('Enter an email completely please. It should be involved domain part.') #Display error message if email is incomplete
     exit() # Exit the program if email is invalid
-(username,domain)=a.split('@') #Split email into username and domain using '@' as separator
+(username,domain)=a.split('@') #Split email into username and domain using '@' as separator ( we use 'tuple' structure for this line.)
 print('Username=',username) # print the username
 print('Domain=',domain) # print the domain
